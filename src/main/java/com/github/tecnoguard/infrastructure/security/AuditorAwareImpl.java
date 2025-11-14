a99eba25ad8773086aa63cce2b4624db6c517099
eduardo <ebrauta@gmail.com>
2025-11-13 22:33:46 -0300

    feat: criação de auditoria simplificada, ajustes no teste devido a auditoria

---
+++ src/main/java/com/github/tecnoguard/infrastructure/security/AuditorAwareImpl.java
@@ -0,0 +1,17 @@
+package com.github.tecnoguard.infrastructure.security;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+@Component
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return Optional.of(auth != null ? auth.getName() : "SYSTEM");
+    }
+}
